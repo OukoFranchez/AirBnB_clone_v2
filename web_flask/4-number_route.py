897803a30170e98e59f080bb67cdd9abc2f4cb1b
OukoFranchez <96543749+OukoFranchez@users.noreply.github.com>
2023-12-26 17:27:36 +0300

    add solution to task 5

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -14,15 +14,18 @@ from flask import abort
 
 app = Flask(__name__)
 
+
 @app.route("/", strict_slashes=False)
 def hello_hbnb():
     """Displays 'Hello HBNB!'."""
     return "Hello HBNB!"
 
+
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
     """Displays 'HBNB'."""
     return "HBNB"
+
 
 @app.route("/c/<text>", strict_slashes=False)
 def c(text):
@@ -32,6 +35,7 @@ def c(text):
     text = text.replace("_", " ")
     return "C {}".format(text)
 
+
 @app.route("/python", strict_slashes=False)
 @app.route("/python/<text>", strict_slashes=False)
 def python(text="is cool"):
@@ -40,6 +44,7 @@ def python(text="is cool"):
     """
     text = text.replace("_", " ")
     return "Python {}".format(text)
+
 
 @app.route("/number/<int:n>", strict_slashes=False)
 def number(n):
